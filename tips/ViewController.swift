d1ff37de70ff66077bee89290969c05093756f2a
Sumeet Shendrikar <sumeetshendrikar@springfield.attlocal.net>
2015-09-13 23:12:24 -0700

    First commit

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        tipPctSliderLabel.text = "\(Int(round(tipPctSlider.value)))%"
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var tipAmountSelector: UISegmentedControl!
+    
+    var tipAmountPct:Double = 0.18
+    
+    @IBAction func tipAmountChanged(sender: AnyObject) {
+//        println("tipAmount selector \(tipAmountSelector.selectedSegmentIndex)")
+        switch tipAmountSelector.selectedSegmentIndex {
+        case 0:
+            tipAmountPct = 0.18
+        case 1:
+            tipAmountPct = 0.20
+        case 2:
+            tipAmountPct = 0.25
+        case UISegmentedControlNoSegment:
+            println("no segment selected!")
+            fallthrough
+        default:
+            tipAmountPct = 0.18
+        }
+        updateFieldsOld()
+    }
+    
+    func updateFieldsOld() {
+        var billAmount = (billField.text as NSString).doubleValue
+        //        println("user entered \(billAmount)")
+        var tipAmount = billAmount * tipAmountPct
+        tipLabel.text = String(format: "$%.2f",
+            tipAmount)
+        totalLabel.text = String(format: "$%.2f", (billAmount + tipAmount))
+    }
+    
+    
+    @IBOutlet weak var tipPctSliderLabel: UILabel!
+    
+    @IBOutlet weak var tipPctSlider: UISlider!
+    
+    @IBAction func onTipPctSliderChanged(sender: AnyObject) {
+            tipPctSliderLabel.text = "\(Int(round(tipPctSlider.value)))%"
+        updateFields()
+    }
+    
+    func updateFields() {
+        var billAmount = (billField.text as NSString).doubleValue
+        var tipAmount = billAmount * (Double)(round(tipPctSlider.value)/100.0)
+        tipLabel.text = String(format: "$%.2f",
+            tipAmount)
+        totalLabel.text = String(format: "$%.2f", (billAmount + tipAmount))
+    }
+    
+    
+    
+    @IBAction func onEditingChanged(sender: AnyObject) {
+ //       println("user editing bill")
+        updateFields()
+    }
 }
 
